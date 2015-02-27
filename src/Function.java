605c6f27dd5a5a24b54a7b7af48baaacfe7d572c
unknown <C:\Users\Rezenter\AppData\Roaming\The Bat!\Mail>
2015-02-27 23:07:49 +0300

    Initial commit

---
+++ src/Function.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Function{
+    final static long MOD = (long) Math.pow(2, 32);
+
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner in = new Scanner(new File("function.in"));
+        long x = (long)in.nextInt();
+        in.close();
+
+        PrintWriter pw = new PrintWriter(new File("function.out"));
+        pw.print((func(x)));
+        pw.close();
+    }
+
+    static Map<Long, Long> done = new HashMap<Long, Long>();
+
+    public static long func(long x){
+        if(done.containsKey(x)){
+            return done.get(x);
+        }
+        if(x <= 2){
+            done.put(x, (long) 1);
+        }else{
+            if(x % 2 == 1){
+                done.put(x, (func(6 * x / 7)%MOD + func(2 * x / 3)%MOD)%MOD);
+            }else{
+                done.put(x,(func(x - 1)%MOD + func(x - 3)%MOD)%MOD);
+            }
+        }
+        return done.get(x);
+    }
+}
