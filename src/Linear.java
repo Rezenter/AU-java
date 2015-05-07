22771e289b9e4469db3c35b1e14779fb93acf7d4
Rezenter <nisovru@gmail.com>
2015-05-07 23:28:28 +0300

    third (or fourth?) contest 11.04.2015

--- src/Linear.java
+++ src/Linear.java
@@ -1,6 +1,6 @@
 /**
  * Created by Rezenter on 24.04.2015.
- * fails test 24
+ * fails test 30
  */
 
 import java.io.FileNotFoundException;
@@ -11,32 +11,33 @@ import java.io.PrintWriter;
 import java.io.File;
 
 public class Linear {
-    public static final double EPS = 0.00005;
+    public static final double EPS = 0.000000000001;
+
     public static void main(String[] args) throws FileNotFoundException {
-            String res = "";
-            try {
-                Scanner in = new Scanner(new File("linear.in"));
-                int n = in.nextInt();
-                in.nextLine();
-                Matrix matrix = new Matrix(n, n + 1);
-                Matrix clone = new Matrix(n, n + 1);
-                for (int i = 0; i < n; i++) {
-                    for (int l = 0; l < n + 1; l++) {
-                        double tmp = (double) in.nextInt();
-                        matrix.set(i, l, tmp);
-                        clone.set(i, l, tmp);
-                    }
+        String res = "";
+        try {
+            Scanner in = new Scanner(new File("linear.in"));
+            int n = in.nextInt();
+            in.nextLine();
+            Matrix matrix = new Matrix(n, n + 1);
+            Matrix clone = new Matrix(n, n + 1);
+            for (int i = 0; i < n; i++) {
+                for (int l = 0; l < n + 1; l++) {
+                    double tmp = (double) in.nextInt();
+                    matrix.set(i, l, tmp);
+                    clone.set(i, l, tmp);
                 }
-                in.close();
-                res = verdict(solve(matrix), matrix);
-            }catch(Exception e){
-                res = "internal error";
             }
+            in.close();
+            res = verdict(solve(matrix), matrix);
+        } catch (Exception e) {
+            res = "internal error";
+        }
 
 
-            PrintWriter pw = new PrintWriter(new File("linear.out"));
-            pw.print(res);
-            pw.close();
+        PrintWriter pw = new PrintWriter(new File("linear.out"));
+        pw.print(res);
+        pw.close();
     }
 
     public static double[] solve(Matrix mat) {
@@ -44,45 +45,47 @@ public class Linear {
         double[] x = new double[n];
         boolean flag = false;
 
-        for(int i = 0;i < n; i ++){
-            if(mat.pivote(i) == i && Math.abs(mat.getMat()[i][i]) <= EPS){
+        for (int i = 0; i < n; i++) {
+            if (mat.pivote(i) == i && Math.abs(mat.getMat()[i][i]) <= EPS) {
                 flag = true;
             }
             mat.swapStrings(i, mat.pivote(i));
             mat.extract(i);
         }
 
-        for(int i = n - 1; i >= 0; i--){
+        for (int i = n - 1; i >= 0; i--) {
             mat.extractRev(i);
         }
 
-        for(int i = 0;i < n; i ++){
-            x[i] = mat.getMat()[i][n]/mat.getMat()[i][i];
+        for (int i = 0; i < n; i++) {
+            x[i] = mat.getMat()[i][n] / mat.getMat()[i][i];
         }
 
-        if(flag){
-            for(int i = 0; i< n ; i++){
-                if(Math.abs(mat.getMat()[i][i]) <= EPS && Math.abs(mat.getMat()[i][n]) > EPS){
+        if (flag) {
+            for (int i = 0; i < n; i++) {
+                if (Math.abs(mat.getMat()[i][i]) <= EPS && Math.abs(mat.getMat()[i][n]) > EPS) {
                     x[0] = Double.NaN;
                     return x;
                 }
             }
-            return new double[n+1];
+            return new double[n + 1];
         }
         return x;
     }
 
-    public static String verdict(double[] x, Matrix mat){
+    public static String verdict(double[] x, Matrix mat) {
         int n = mat.getMat().length;
         String res = "single\n";
-        if(x.length != n){
+        if (x.length != n) {
             return "infinity";
         }
-        if(Math.abs(mat.getMat()[n-1][n]) <= EPS && Math.abs(mat.getMat()[n-1][n-1]) <= EPS){
-            return "infinity";
+        for (int i = 0; i < n; i++) {
+            if (Math.abs(mat.getMat()[i][n]) <= EPS && Math.abs(mat.getMat()[i][i]) <= EPS) {
+                return "infinity";
+            }
         }
         for (int i = 0; i < x.length; i++) {
-            if(Double.isInfinite(x[i]) || Double.isNaN(x[i])){
+            if (Double.isInfinite(x[i]) || Double.isNaN(x[i])) {
                 return "impossible";
             }
             res += new BigDecimal(x[i]).setScale(3, RoundingMode.HALF_UP).doubleValue() + " ";
@@ -120,35 +123,35 @@ public class Linear {
             mat[i][j] = elem;
         }
 
-        public void swapStrings(int f, int s){
+        public void swapStrings(int f, int s) {
             double[] tmp = mat[f];
             mat[f] = mat[s];
             mat[s] = tmp;
         }
 
-        public void extract(int start){
-            for(int i = start + 1; i < height; i ++){
-                double dif = mat[i][start]/mat[start][start];
-                for(int j = start; j < wide; j ++){
-                    this.set(i,j, mat[i][j] - (mat[start][j] * dif));
+        public void extract(int start) {
+            for (int i = start + 1; i < height; i++) {
+                double dif = -mat[i][start] / mat[start][start];
+                for (int j = start; j < wide; j++) {
+                    this.set(i, j, mat[i][j] + (mat[start][j] * dif));
                 }
             }
         }
 
-        public void extractRev(int finish){
-            for(int i = finish - 1; i >= 0; i--){
-                double dif = mat[i][finish]/mat[finish][finish];
-                for(int j = finish; j < wide; j ++){
-                    this.set(i,j, mat[i][j] - (mat[finish][j] * dif));
+        public void extractRev(int finish) {
+            for (int i = finish - 1; i >= 0; i--) {
+                double dif = -mat[i][finish] / mat[finish][finish];
+                for (int j = finish; j < wide; j++) {
+                    this.set(i, j, mat[i][j] + (mat[finish][j] * dif));
                 }
             }
         }
 
-        public int pivote(int column){
+        public int pivote(int column) {
             int res = column;
             double max = 0;
-            for(int i = column; i < height; i++){
-                if(mat[i][column] > max){
+            for (int i = column; i < height; i++) {
+                if (mat[i][column] > max) {
                     max = mat[i][column];
                     res = i;
                 }
@@ -156,13 +159,13 @@ public class Linear {
             return res;
         }
 
-        public Matrix mult(int c, int d){
+        public Matrix mult(int c, int d) {
             Matrix res = new Matrix(height, wide);
-            for(int i = 0; i< height; i++) {
+            for (int i = 0; i < height; i++) {
                 for (int k = 0; k < wide; k++) {
-                    if(k != wide - 1){
+                    if (k != wide - 1) {
                         res.push(c * mat[i][k]);
-                    }else{
+                    } else {
                         res.push(d * mat[i][k]);
                     }
                 }
--- src/Puzzle.java
+++ src/Puzzle.java
@@ -12,80 +12,69 @@ public class Puzzle {
         Scanner in = new Scanner(new File("puzzle.in"));
         int n = in.nextInt();
         in.nextLine();
-        Matrix matrix = new Matrix(n, n);
+        Matrix tmp = new Matrix(n, n);
         for(int i = 0; i < n; i++){
             for(int l = in.nextInt(); l > 0; l--){
-                matrix.set(i, in.nextInt()-1, 1);
+                tmp.set(i, in.nextInt() - 1, 1);
             }
         }
 
         int[] defaults = new int[n];
         for(int i = 0; i < n; i++){
-            defaults[i] = -in.nextInt();
+            defaults[i] = in.nextInt();
         }
         in.close();
-        int[] solvation = solve(matrix.getMat(), defaults);
+        Matrix matrix = new Matrix(n, n+1);
+        for(int i = 0; i < n; i++){
+            for(int l = 0; l < n; l++){
+                matrix.set(i,l,tmp.getMat()[i][l]);
+            }
+            matrix.set(i, n, defaults[i]);
+        }
+        int[] x = solve(matrix, defaults);
         String res = "";
-        if(solvation[0] == -2){
-            res = "-11";
-        }else {
+        String temp = "";
+        if(x[0] == -100){
+            res = "-1";
+        }else{
             int count = 0;
-            for(int i = 0; i < solvation.length; i++){
-                if(Math.abs(solvation[i])%2 == 1){
+            for(int i = 0; i < x.length; i++){
+                if(x[i] == 1){
                     count ++;
+                    temp += (i+1) + " ";
                 }
             }
-            res += count + "\n";
-            for (int i = 0; i < solvation.length; i++) {
-                if(Math.abs(solvation[i])%2 == 1){
-                    res += i+1 + " ";
-                }
-            }
+            res += count+ "\n" + temp;
+            res = res.substring(0,res.length()-1);
         }
         PrintWriter pw = new PrintWriter(new File("puzzle.out"));
-        pw.print(res.substring(0, res.length() - 1));
+        pw.print(res);
         pw.close();
     }
-
-    private static final int EPSILON = 0;
     
-    public static int[] solve(int[][] mat, int[] b) {
-        int n  = b.length;
-
-        for (int p = 0; p < n; p++) {
-            int max = p;
-            for (int i = p + 1; i < n; i++) {
-                if (Math.abs(mat[i][p]) > Math.abs(mat[max][p])) {
-                    max = i;
-                }
-            }
-            int[] temp = mat[p];
-            mat[p] = mat[max];
-            mat[max] = temp;
-            int t = b[p];
-            b[p] = b[max];
-            b[max] = t;
-            if (Math.abs(mat[p][p]) <= EPSILON) {
-                int[] error = new int[1];
-                error[0] = -2;
-                return error;
-            }
-            for (int i = p + 1; i < n; i++) {
-                int alpha = (mat[i][p] / mat[p][p]);
-                b[i] -= (alpha * b[p]);
-                for (int j = p; j < n; j++) {
-                    mat[i][j] -= alpha * mat[p][j];
-                    mat[i][j] = mat[i][j]%2;
-                }
-            }
-        }
+    public static int[] solve(Matrix mat, int[] b) {
+        int n = mat.getMat().length;
         int[] x = new int[n];
-        for (int i = n - 1; i >= 0; i--) {
-            int sum = 0;
-            for (int j = i + 1; j < n; j++) {
-                sum += mat[i][j] * x[j];
+
+        for(int i = 0;i < n; i ++){
+            mat.swapStrings(i, mat.pivote(i));
+            mat.extract(i);
+        }
+
+        for(int i = n - 1; i >= 0; i--){
+            mat.extractRev(i);
+        }
+
+        for(int i = 0;i < n; i ++){
+            if(mat.getMat()[i][i] == 0){
+                if(mat.getMat()[i][n] == 0){
+                    x[i]= 0;
+                }else{
+                    x[0]=-100;
+                }
+            }else{
+                x[i] = (mat.getMat()[i][n]/mat.getMat()[i][i])%2;
             }
-            x[i] = (b[i] - sum) / mat[i][i];
         }
         return x;
     }
@@ -97,13 +86,41 @@ public class Puzzle {
         int wide;
         int[][] mat;
 
+        public void extract(int start){
+            for(int i = start + 1; i < height; i ++){
+                if(mat[i][start] != 0){
+                    for(int j = start; j < wide; j ++) {
+                        this.set(i, j, Math.abs((mat[i][j] - mat[start][j]) % 2));
+                    }
+                }
+            }
+        }
+
+        public void extractRev(int finish){
+            for(int i = finish - 1; i >= 0; i--){
+                if(mat[i][finish] != 0){
+                for(int j = finish; j < wide; j ++){
+                    this.set(i,j, Math.abs((mat[i][j] - mat[finish][j])%2));
+                }}
+            }
+        }
+
+        public int pivote(int column){
+            for(int i = column; i < height; i++){
+                if(mat[i][column] > 0){
+                    return i;
+                }
+            }
+            return column;
+        }
+
         Matrix(int n ,int  m){
             height = n;
             wide = m;
             mat = new int[n][m];
         }
 
-        public void push(Integer elem){
+        public void push(int elem){
             if(v == wide && h == height + 1){
             }else{
                 mat[h][v] = elem;
@@ -130,6 +147,12 @@ public class Puzzle {
                 res += "\n";
             }
             return res.substring(0,res.length()-1);
+        }
+
+        public void swapStrings(int f, int s){
+            int[] tmp = mat[f];
+            mat[f] = mat[s];
+            mat[s] = tmp;
         }
 
         public int getWide(){
@@ -187,5 +210,6 @@ public class Puzzle {
             }
             return res;
         }
+
     }
 }
